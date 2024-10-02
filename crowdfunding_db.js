7dff2480730369a4d26a3d4b6e8c51561f84a710
liwei2269 <2269055534@qq.com>
2024-10-02 15:33:51 +0800

    A2
    WebA2

--- crowdfunding_db.js
+++ crowdfunding_db.js
@@ -73,4 +73,8 @@ app.get('/search', (req, res) => {
     res.json(results);
   });
 });
-
+//Create a RESTful API that allows clients to query crowdfunding campaigns and their details while supporting simple search filtering.
+const PORT = 3333;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
